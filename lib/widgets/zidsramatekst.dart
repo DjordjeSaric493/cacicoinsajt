abef203c83bff1677fb58eb6dae23b8874bb3a02
ZigiStardast <129152646+ZigiStardast@users.noreply.github.com>
2025-04-13 14:14:10 +0200

    upeglan text zidsrama

--- lib/widgets/zidsramatekst.dart
+++ lib/widgets/zidsramatekst.dart
@@ -18,7 +18,7 @@ class ZidSramaText extends StatelessWidget {
         Text('Zid srama', style: deliciousTextStyleBigRed),
         SizedBox(height: isMobile ? 6 : 8),
         Text(
-          'Prst na čelo ćaci, na zidu si, a voleo bi da nisi!\nDobra vest je da postoji način, loša vest za tebe - nema\n “kozmetike”, zahteva suštinu.Nepoznata teritorija,\n hmmm…jeste, ali ne brini!\nPokaži za početak dovoljno skromnosti da prihvatiš činjenično\n stanje, preuzmi odgovornost za situaciju u kojoj se nalaziš,\n pronađi hrabrosti u sebi za potrebnu inicijativu u delima koja ti\n pomažu da se suočiš sa svim izazovima, i ovo mesto ovde će ti\n ukazati neverovatnu moć izbora!',
+          'Prst na čelo ćaci, na zidu si, a voleo bi da nisi!\nDobra vest je da postoji način, loša vest za tebe - nema\n “kozmetike”, zahteva suštinu.Nepoznata teritorija,\n hmmm…jeste, ali ne brini!\nPokaži za početak dovoljno skromnosti da prihvatiš činjenično stanje, preuzmi odgovornost za situaciju u kojoj se nalaziš, pronađi hrabrosti u sebi za potrebnu inicijativu u delima koja ti\n pomažu da se suočiš sa svim izazovima, i ovo mesto ovde će ti  ukazati neverovatnu moć izbora!',
           style: dekkoTextStyle,
           textAlign: TextAlign.center,
         ),
